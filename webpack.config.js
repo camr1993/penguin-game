dfb083e4ff5e595f9256d24394f0900a49a0d8b3
Cameron Ratliff <cratliff@colgate.edu>
2020-04-15 20:58:45 -0400

    setup done

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+// need to require because it's used for graphics plugins!
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+
+  // devServer: {
+  //   contentBase: './public',
+  //   inline: true,
+  //   hot: true
+  // },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    // browser rendering engines used by Phaser.AUTO (in Phaser config file)
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+  ],
+};
