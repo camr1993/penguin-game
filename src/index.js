78f5e01b2854dd30de2b049013b932c5fd0760f7
Cameron Ratliff <cratliff@colgate.edu>
2020-04-16 20:44:35 -0400

    gun added

--- src/index.js
+++ src/index.js
@@ -10,10 +10,11 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 600 },
       debug: false,
     },
   },
+  // this is saying there is one scene, and you are importing preload/create/update for that one scene
   scene: {
     preload: preload,
     create: create,
@@ -26,11 +27,16 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('ground', 'assets/platform.png');
+  this.load.image('pistol', 'assets/pistol.png');
   this.load.image('star', 'assets/star.png');
   this.load.image('bomb', 'assets/bomb.png');
+  // this.load.spritesheet('penguin', 'assets/penguin_old.png', {
+  //   frameWidth: 18,
+  //   frameHeight: 21,
+  // });
   this.load.spritesheet('penguin', 'assets/penguin.png', {
-    frameWidth: 18,
-    frameHeight: 21,
+    frameWidth: 64,
+    frameHeight: 75,
   });
 }
 
@@ -43,6 +49,8 @@ let bombs;
 let score = 0;
 let scoreText;
 let gameOver = false;
+let facingLeft = false;
+let holdingWeapon = false;
 
 function create() {
   // background:
@@ -58,39 +66,40 @@ function create() {
 
   // player:
   player = this.physics.add.sprite(100, 450, 'penguin');
-  player.setScale(2.0);
-  // player.setBounce(0.2);
+  player.setScale(0.75);
   player.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('penguin', { start: 4, end: 7 }),
-    frameRate: 10,
-    repeat: -1,
-  });
 
   this.anims.create({
     key: 'stop',
     frames: [{ key: 'penguin', frame: 0 }],
     frameRate: 20,
   });
-
   this.anims.create({
-    key: 'right',
+    key: 'run',
     frames: this.anims.generateFrameNumbers('penguin', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
-
   this.anims.create({
-    key: 'jumpRight',
-    frames: [{ key: 'penguin', frame: 9 }],
+    key: 'jump',
+    frames: [{ key: 'penguin', frame: 4 }],
     frameRate: 20,
   });
 
   this.anims.create({
-    key: 'jumpLeft',
-    frames: [{ key: 'penguin', frame: 8 }],
+    key: 'stopPistol',
+    frames: [{ key: 'penguin', frame: 7 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: 'runPistol',
+    frames: this.anims.generateFrameNumbers('penguin', { start: 7, end: 10 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: 'jumpPistol',
+    frames: [{ key: 'penguin', frame: 11 }],
     frameRate: 20,
   });
 
@@ -116,41 +125,42 @@ function create() {
   // bombs:
   bombs = this.physics.add.group();
 
+  // pistols:
+  let pistols = this.physics.add.group();
+  let pistol = pistols.create(330, 300, 'pistol');
+  // pistol.setScale(0.1);
+
   // collisions:
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
   this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(pistols, platforms);
 
   this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player, pistols, pickupPistol, null, this);
   this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update() {
-  // jumping from ground
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-  }
+  jumpFromGround(cursors, player);
+  leftMovements(cursors, player);
+  rightMovements(cursors, player);
 
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    if (player.body.touching.down) {
-      player.anims.play('left', true);
-    } else {
-      player.anims.play('jumpLeft');
-    }
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-    if (player.body.touching.down) {
-      player.anims.play('right', true);
-    } else {
-      player.anims.play('jumpRight');
-    }
-  } else {
+  if (!cursors.left.isDown && !cursors.right.isDown) {
+    //
     player.setVelocityX(0);
     if (player.body.touching.down) {
-      player.anims.play('stop');
+      if (holdingWeapon) {
+        player.anims.play('stopPistol');
+      } else {
+        player.anims.play('stop');
+      }
     } else {
-      player.anims.play('jumpRight');
+      if (holdingWeapon) {
+        player.anims.play('jumpPistol');
+      } else {
+        player.anims.play('jump');
+      }
     }
   }
 }
@@ -187,4 +197,61 @@ function hitBomb(player, bomb) {
   player.setTint(0xff0000);
   player.anims.play('stop');
   gameOver = true;
+}
+
+function pickupPistol(player, pistol) {
+  pistol.disableBody(true, true);
+  holdingWeapon = true;
+}
+
+function jumpFromGround(cursors, player) {
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-500);
+  }
+}
+
+function leftMovements(cursors, player) {
+  if (cursors.left.isDown) {
+    if (!facingLeft) {
+      player.flipX = !player.flipX;
+      facingLeft = true;
+    }
+    player.setVelocityX(-210);
+    if (player.body.touching.down) {
+      if (holdingWeapon) {
+        player.anims.play('runPistol', true);
+      } else {
+        player.anims.play('run', true);
+      }
+    } else {
+      if (holdingWeapon) {
+        player.anims.play('jumpPistol', true);
+      } else {
+        player.anims.play('jump');
+      }
+    }
+  }
+}
+
+function rightMovements(cursors, player) {
+  if (cursors.right.isDown) {
+    if (facingLeft) {
+      player.flipX = !player.flipX;
+      facingLeft = false;
+    }
+    player.setVelocityX(210);
+    if (player.body.touching.down) {
+      if (holdingWeapon) {
+        player.anims.play('runPistol', true);
+      } else {
+        player.anims.play('run', true);
+      }
+    } else {
+      if (holdingWeapon) {
+        player.anims.play('jumpPistol', true);
+      } else {
+        player.anims.play('jump');
+      }
+    }
+  }
 }
